103aa760a129dce131c9efe74882cff0fe0f43ea
harini90work <78424782+harini90work@users.noreply.github.com>
2021-07-19 17:48:05 +0530

    Add files via upload

--- scanner/Img_Proc.py
+++ scanner/Img_Proc.py
@@ -2,97 +2,8 @@ import cv2
 import cvzone
 import numpy as np
 import os
-#%% Subfunctions
 
-def blankimg(height, width, rgb_color=(0, 255, 0)):
-    """Create new image(numpy array) filled with certain color in RGB"""
-    # Create black blank image
-    image = np.zeros((height, width, 3), np.uint8)
-
-    # Since OpenCV uses BGR, convert the color first
-    color = tuple(reversed(rgb_color))
-    # Fill image with color
-    image[:] = color
-
-    return image
-
-
-def print_coordinate(event, x, y, flags, param):
-    if event == cv2.EVENT_LBUTTONDOWN:
-        print(x,y)
-
-def printxy(img):
-    window_name = "Find Coordinate"
-    cv2.namedWindow(window_name)
-    cv2.setMouseCallback(window_name, print_coordinate)
-    display(img,window_name)
-    
-    
-def display(img, name = 'Preview', wait_sec = 0):
-    cv2.imshow(name, img)
-    if cv2.waitKey(wait_sec) & 0xFF == ord('q'):
-        pass
-    cv2.destroyAllWindows()     
-    
-
-def hsv_callback(x):
-    global H_low,H_high,S_low,S_high,V_low,V_high
-    #assign trackbar position value to H,S,V High and low variable
-    H_low = cv2.getTrackbarPos('low H','controls')
-    H_high = cv2.getTrackbarPos('high H','controls')
-    S_low = cv2.getTrackbarPos('low S','controls')
-    S_high = cv2.getTrackbarPos('high S','controls')
-    V_low = cv2.getTrackbarPos('low V','controls')
-    V_high = cv2.getTrackbarPos('high V','controls')
-    
-def hsv_mask_area(img,hsv_low,hsv_high):
-    hsv_low = np.array(hsv_low, dtype = np.uint8)
-    hsv_high = np.array(hsv_high, dtype = np.uint8)
-    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-    hsv[...,1] = hsv[...,1]*0.8
-    mask = cv2.inRange(hsv, hsv_low, hsv_high)
-    res = cv2.bitwise_and(img, img, mask=mask)
-    contors,_ = cv2.findContours(mask,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
-    area = 0
-    box = (0,0,0,0)
-    for cnt in contors:
-        area += cv2.contourArea(cnt)
-        box = cv2.boundingRect(cnt)
-    
-    return mask, res, area, box
-
-
-def hsv_value_detect(img):
-    
-    #create a seperate window named 'controls' for trackbar
-    cv2.namedWindow('controls',2)
-    cv2.resizeWindow("controls", 550,200);
-
-    #create trackbars for high,low H,S,V 
-    cv2.createTrackbar('low H','controls',0,179,hsv_callback)
-    cv2.createTrackbar('high H','controls',179,179,hsv_callback)
-
-    cv2.createTrackbar('low S','controls',0,255,hsv_callback)
-    cv2.createTrackbar('high S','controls',255,255,hsv_callback)
-
-    cv2.createTrackbar('low V','controls',0,255,hsv_callback)
-    cv2.createTrackbar('high V','controls',255,255,hsv_callback)
-    
-    while True:
-        hsv_low = np.array([H_low, S_low, V_low], np.uint8)
-        print(hsv_low)
-        #print(hsv_low)
-        hsv_high = np.array([H_high, S_high, V_high], np.uint8)
-        #making mask for hsv range
-        mask, res, area, box = hsv_mask_area(img, hsv_low, hsv_high)
-        cv2.imshow('mask',mask)
-        cv2.imshow('res',res)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-    cv2.destroyAllWindows()
-    return (mask,area,hsv_low,hsv_high )
-
-
+#Image Functions used in the project
 def resize_image(img, ref_img = None, color = (0,0,0), fx = None, fy = None, interpolation = None):
     if(ref_img is None):
         ref_img = img.copy()
@@ -117,37 +28,6 @@ def resize_image(img, ref_img = None, color = (0,0,0), fx = None, fy = None, int
     
     return image_final
 
-
-
-
-def list_ports():
-    """
-    Test the ports and returns a tuple with the available ports and the ones that are working.
-    """
-    is_working = True
-    dev_port = 0
-    working_ports = []
-    available_ports = []
-    while is_working:
-        camera = cv2.VideoCapture(dev_port)
-        if not camera.isOpened():
-            is_working = False
-            print("Port %s is not working." %dev_port)
-        else:
-            is_reading, img = camera.read()
-            w = camera.get(3)
-            h = camera.get(4)
-            if is_reading:
-                print("Port %s is working and reads images (%s x %s)" %(dev_port,h,w))
-                working_ports.append(dev_port)
-            else:
-                print("Port %s for camera ( %s x %s) is present but does not reads." %(dev_port,h,w))
-                available_ports.append(dev_port)
-        dev_port +=1
-    return available_ports,working_ports
-#%%
-### 2 New functions to be created
-#Resize image
 def readimg(filename):
     if(os.path.isfile(filename)):
         img = cv2.imread(filename)
@@ -160,25 +40,21 @@ def readmask(filename):
     if(img is not None):
         img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     return img
-#Comibing images horizontal 
-
-
-#Comibing images vertically
-def vconcat_resize_min(im_list, interpolation=cv2.INTER_CUBIC):
-    w_min = min(im.shape[1] for im in im_list)
-    im_list_resize = [cv2.resize(im, (w_min, int(im.shape[0] * w_min / im.shape[1])), interpolation=interpolation)
-                      for im in im_list]
-    return cv2.vconcat(im_list_resize)
-
-
 
 def rgb_to_hex(rgb):
     return '%02x%02x%02x' % rgb
 
-H_low = 0
-S_low = 0
-V_low = 0
 
-H_high = 179
-S_high = 255
-V_high = 255
+def blankimg(height, width, rgb_color=(0, 255, 0)):
+    """Create new image(numpy array) filled with certain color in RGB"""
+    # Create black blank image
+    image = np.zeros((height, width, 3), np.uint8)
+
+    # Since OpenCV uses BGR, convert the color first
+    color = tuple(reversed(rgb_color))
+    # Fill image with color
+    image[:] = color
+
+    return image
+    
+#%% Subfunctions
--- scanner/color_extraction.py
+++ scanner/color_extraction.py
@@ -34,90 +34,7 @@ import datetime as dt
 import matplotlib.pyplot as plt
 #%%
 #% Project Specific Functions
-def process_circle(circles, offset1 = 0, offset2 = 0):
-    final_circle = []
-    for (x, y, r) in circles:
-        valid = True
-        grid_id = None
-        if((x>560+offset1) & (x<660-offset2)):
-            if((y>130+offset1) & (y<230-offset2)):
-                grid_id = 'Grid 1;3'
-        if((x>730+offset1) & (x<830-offset2)):
-            if((y>130+offset1) & (y<230-offset2)):
-                grid_id = 'Grid 1;4'
-        if((x>890+offset1) & (x<990-offset2)):
-            if((y>130+offset1) & (y<230-offset2)):
-                grid_id = 'Grid 1;5'
-        if((x>560+offset1) & (x<660-offset2)):
-            if((y>290+offset1) & (y<390-offset2)):
-                grid_id = 'Grid 2;3'
-        if((x>730+offset1) & (x<830-offset2)):
-            if((y>290+offset1) & (y<390-offset2)):
-                grid_id = 'Grid 2;4'
-        if((x>890+offset1) & (x<990-offset2)):
-            if((y>290+offset1) & (y<390-offset2)):
-                grid_id = 'Grid 2;5'
-        if((x>250+offset1) & (x<350-offset2)):
-            if((y>440+offset1) & (y<540-offset2)):
-                grid_id = 'Grid 3;1'
-        if((x>410+offset1) & (x<510-offset2)):
-            if((y>440+offset1) & (y<540-offset2)):
-                grid_id = 'Grid 3;2'
-        if((x>560+offset1) & (x<660-offset2)):
-            if((y>440+offset1) & (y<540-offset2)):
-                grid_id = 'Grid 3;3'
-                
-        if((x>730+offset1) & (x<830-offset2)):
-            if((y>440+offset1) & (y<540-offset2)):
-                grid_id = 'Grid 3;4'
-                
-        if((x>890+offset1) & (x<990-offset2)):
-            if((y>440+offset1) & (y<540-offset2)):
-                grid_id = 'Grid 3;5'
-                
-        if((x>250+offset1) & (x<350-offset2)):
-            if((y>610+offset1) & (y<710-offset2)):
-                grid_id = 'Grid 4;1'
-                
-        if((x>410+offset1) & (x<510-offset2)):
-            if((y>610+offset1) & (y<710-offset2)):
-                grid_id = 'Grid 4;2'
-                
-        if((x>560+offset1) & (x<660-offset2)):
-            if((y>610+offset1) & (y<710-offset2)):
-                grid_id = 'Grid 4;3'
-        if((x>730+offset1) & (x<830-offset2)):
-            if((y>610+offset1) & (y<710-offset2)):
-                grid_id = 'Grid 4;4'
-                
-        if((x>890+offset1) & (x<990-offset2)):
-            if((y>610+offset1) & (y<710-offset2)):
-                grid_id = 'Grid 4;5'
-                
-        if((x>250+offset1) & (x<350-offset2)):
-            if((y>770+offset1) & (y<870-offset2)):
-                grid_id = 'Grid 5;1'
-                
-        if((x>410+offset1) & (x<510-offset2)):
-            if((y>770+offset1) & (y<870-offset2)):
-                grid_id = 'Grid 5;2'
-                
-        if((x>560+offset1) & (x<660-offset2)):
-            if((y>770+offset1) & (y<870-offset2)):
-                grid_id = 'Grid 5;3'
-                
-        if((x>730+offset1) & (x<830-offset2)):
-            if((y>770+offset1) & (y<870-offset2)):
-                grid_id = 'Grid 5;4'
-                
-        if((x>890+offset1) & (x<990-offset2)):
-            if((y>770+offset1) & (y<870-offset2)):
-                grid_id = 'Grid 5;5'
 
-                
-        if(grid_id is not None):
-            final_circle.append((x,y,r,grid_id))
-    return final_circle
 def extract_circle(img, r = 35):
     circle_list = [(585,200,r,'Grid 1;3'),
                    (730,200,r,'Grid 1;4'),
@@ -155,35 +72,15 @@ def detect_color(main_img, original_img = None, factor = None):
     if(original_img is None):
         original_img = main_img.copy()
         
-    #img_final1 = main_img.copy()
-    #img_final1 = im.resize_image(img_final1,fx=2,fy=2, interpolation = cv2.INTER_CUBIC) #cv2.resize(img_final1, (0,0), fx=2, fy=2,interpolation = cv2.INTER_CUBIC)
     op_img_final = im.resize_image(original_img,fx=2,fy=2, interpolation = cv2.INTER_CUBIC)
     #%
     circles = extract_circle(op_img_final)
     cv2.imwrite('./input_image_considered.png',op_img_final)
-    #extract_circle(op_img_final)
-    #gray = cv2.cvtColor(img_final1, cv2.COLOR_BGR2GRAY)
-    
-    
-    # detect circles in the image
-    
-    # ensure at least some circles were found
-    #hsv_low = [  0,   0, 255]
-    #hsv_high = [0 , 0, 255]
-    #mask, res, area, box = im.hsv_mask_area(img_final1, hsv_low, hsv_high)
-    #count = 1
     
     output = op_img_final.copy()
     detected_circle = 0
-    #for res in np.arange(1.3,2,0.1):
-         
-    #    circles = cv2.HoughCircles(gray, cv2.HOUGH_GRADIENT, res, 80)
         
     if circles is not None:
-        # convert the (x, y) coordinates and radius of the circles to integers
-        #circles = np.round(circles[0, :]).astype("int")
-        #circles = process_circle(circles,0,0)
-        #print(len(circles))
         detected_circle = len(circles)
                         
         if(len(circles)==21):
@@ -199,14 +96,11 @@ def detect_color(main_img, original_img = None, factor = None):
                 #print(x,y,r)
                 if(r>40):
                     r = 40
-                #im.display(output)
-         #       cv2.rectangle(output, (x - r, y - r), (x + r, y + r), (0, 128, 255), -1)
                 cropped = output[y-r+20:y+r-20,x-r+20:x+r-20]
                 cropped = factor*cropped
                 cropped[cropped>255] = 255
                 cropped = cropped.astype('uint8')
                 cropped = cv2.blur(cropped, (13,13))
-                #im.display(cropped1)
                 output[y-r+20:y+r-20,x-r+20:x+r-20] = cropped
                 #
                 rgb = tuple(np.median(cropped,axis=1)[0].astype(int))
@@ -225,16 +119,7 @@ def detect_color(main_img, original_img = None, factor = None):
                 final_result = final_result.append(row,ignore_index=True)
                 count += 1
                 cropped = cv2.putText(output,hex_color,(x-30,y+5),cv2.FONT_HERSHEY_COMPLEX_SMALL,1,(0,0,0))
-                #print(hex_color)
-                #im.display(cropped)
-            #if(max(final_result['r']) > 42):
-            #    output = old_output.copy()
-            #else:
-                #break
-            #    pass
-            # show the output image
-            #im.display(output)
-            #output = cv2.cvtColor(output,cv2.qCOLOR_RGB2BGR)
+    
     if(len(circles)>0):
         for (x, y, r, grid_id) in circles:
             if(r>40):
@@ -249,35 +134,25 @@ def colordetector(ip_img):
     #
     final_result = pd.DataFrame()
     original_img = ip_img.copy()
-    #ip_img = im.readimg('20210714_092722.jpg')
     mask1 = im.readmask('./static/assets/img/masks/mask1.png')
     mask2 = im.readmask('./static/assets/img/masks/mask2.png')
     mask3 = im.readmask('./static/assets/img/masks/mask3.png')
     mask4 = cv2.bitwise_xor(mask2, mask3)
     
-    #ip_img = im.resize_image(ip_img,mask1,interpolation = cv2.INTER_CUBIC)
-    #im.display(mask3)
     main_img = cv2.bitwise_and(ip_img,ip_img,mask=mask1)
     outer_circle = cv2.bitwise_and(ip_img,ip_img,mask=mask2)
     inner_circle = cv2.bitwise_and(ip_img,ip_img,mask=mask3)
+
     #Check Inner Circle Median Color
     contours, hierarchy = cv2.findContours(mask3, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-    #print(contours)
     if(len(contours)>0):
         rect = cv2.boundingRect(contours[0])
         test = inner_circle[rect[1]+10:rect[1]+rect[3]-10,rect[0]+10:rect[0]+rect[2]-10]
-    
-    
-        #im.display(test)
         test1 = np.mean(test,axis=1).mean()
         print('Inner Circle Median color', test1)
     else:
         test1 = 0
-    mark = 0
-    mark = int(((test1/165)/2)*100)
-    if(mark>50):
-        mark = 50
-    print(test1)
+
     innercircle_intensity = int(test1)
     if(test1>=160):
         test1 = True
@@ -289,26 +164,19 @@ def colordetector(ip_img):
     else:
         test1 = False
     outercircle_intensity  = 255
+
     #Check Outer Circle Median Color
-    #test1 = True
     if(test1):
         contours, hierarchy = cv2.findContours(mask4, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
         rect = cv2.boundingRect(contours[0])
         test = outer_circle[rect[1]+10:rect[1]+rect[3]-10,rect[0]+10:rect[0]+rect[2]-10]
         overlay = mask4[rect[1]+10:rect[1]+rect[3]-10,rect[0]+10:rect[0]+rect[2]-10]
         test = cv2.bitwise_and(test,test,mask=overlay)
-        #im.display(test)
         testhsv = cv2.cvtColor(test,cv2.COLOR_BGR2HSV)
         test2 = np.mean(testhsv[:,:,2],axis=1).mean()
         print('Outer Circle Median color', test2)
         outercircle_intensity  = test2
-        if(test2>=0):
-            outercircle_intensity = int(test2)
-            temp = 30/test2
-            temp = int((temp/2) *100)
-            if(temp>50):
-                temp = 50
-            mark = mark + temp
+
         if(test2<=37):
             test2 = True
         else:
@@ -316,81 +184,37 @@ def colordetector(ip_img):
     else:
         test2 = False
     op_img = ip_img.copy()
-    #print(test1, test2)
-    
-        
     
     if(test2):
         print('Final Factor used', factor)
         op_img, final_result = detect_color(main_img, ip_img, factor)
         if(final_result.shape[0]>0):
             final_result = final_result.sort_values('Grid ID')
-        #print(final_result)
-        
     else:
         cv2.putText(op_img,'Keep Image within frame',(10,20),cv2.FONT_HERSHEY_COMPLEX,1,(255,255,0))
         cv2.putText(op_img,f'Inner Circle Intensity: {innercircle_intensity}',(10,60),cv2.FONT_HERSHEY_COMPLEX,1,(255,255,0))
         cv2.putText(op_img,f'Outter Circle Intensity: {outercircle_intensity}',(10,90),cv2.FONT_HERSHEY_COMPLEX,1,(255,255,0))
         
-        #im.display(op_img)
-    #im.display(main_img)
-    #im.display(outer_circle)
-    #im.display(inner_circle)
     op_img = im.resize_image(op_img, original_img, interpolation = cv2.INTER_CUBIC)
     return op_img, final_result
 
 
-#% Test bench
-#ip_file = './input_image_considered'
-#ip_img = im.readimg(f'./{ip_file}.png')
-#ip_img = im.resize_image(ip_img,fx = 0.5, fy=0.5, interpolation = cv2.INTER_CUBIC)
-#op_img, final_result = colordetector(ip_img)
-#im.display(op_img)
-#ip_file = 'ip_img-r3'
-#ip_img = im.readimg(f'./{ip_file}.png')
-#print(dt.datetime.now())
-#test = colordetector(ip_img)
-#im.display(test[0])
-#cv2.imwrite(f'./{ip_file}-op.png', test[0])
-#test[1].to_csv(f'./{ip_file}-op.csv')
-
-#test1 = cv2.cvtColor(test,cv2.COLOR_BGR2HSV)
-#plt.boxplot(np.mean(test[:,:,:],axis=1))
-#%
-#plt.boxplot(np.mean(test1[:,:,2],axis=1))
 
 #%%
 def col_detect_main(ip_img):
-    #video = cv2.VideoCapture(0)
-    #%
     status = False
     mask4 = im.readimg('./static/assets/img/masks/mask4.png')
     mask5 = im.readimg('./static/assets/img/masks/mask5.png')
     ip_img = im.resize_image(ip_img,mask4,interpolation = cv2.INTER_CUBIC)
-    #print(mask4)
     mask6 = cv2.add(mask4, mask5)
     mask6 = cv2.cvtColor(mask6, cv2.COLOR_BGR2GRAY)
     overlay = im.blankimg(mask6.shape[0], mask6.shape[1])
     overlay = cv2.bitwise_and(overlay, overlay, mask = mask6)
     frame = ip_img.copy()
-    #while True:
-    #    status, frame = video.read()
-        
-        #print(status)
-    #    if(status):
-    test = cv2.bitwise_xor(overlay,frame)
     output, df = colordetector(frame)
     output = cv2.bitwise_xor(overlay,output)
     if(df.shape[0] == 21):
         status = True
-            #print(df.shape)
-    #        cv2.imshow('color detector', test)
-    #        cv2.imshow('output', output)
-    #    if cv2.waitKey(1) & 0xFF == ord('q'):
-    #        break
-        
-    #cv2.destroyAllWindows()
-    #im.display(output)
     return output, df, status
     
 print('All Functions Loaded successfully')
