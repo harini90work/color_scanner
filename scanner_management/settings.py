2c36ffde652891bf465598aa03cdde1bbc7956d9
harini90work <78424782+harini90work@users.noreply.github.com>
2021-07-19 13:42:32 +0530

    Add files via upload

--- scanner_management/settings.py
+++ scanner_management/settings.py
@@ -20,8 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-
-SECRET_KEY = environ['secret_key']
+SECRET_KEY = 'django-insecure-5zpm0=i49vn_)v3#2cn4gxe)l&&$e3kol07-q81ou#x+!=hg)g'
+#SECRET_KEY = environ['secret_key']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
