ac24a53a020d2ebe9dfe99958c1288ce28761500
harini90work <78424782+harini90work@users.noreply.github.com>
2021-08-20 16:40:55 +0530

    Add files via upload

--- scanner_management/views.py
+++ scanner_management/views.py
@@ -3,5 +3,19 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
+    '''
+    Main Home function to display the web page
+
+    Parameters
+    ----------
+    request : TYPE
+        DESCRIPTION.
+
+    Returns
+    -------
+    TYPE
+        Render the Index.html.
+
+    '''
     return render(request, 'index.html')
 
